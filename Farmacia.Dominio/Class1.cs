6a0d17b61c0948467c2ba2459fcb5b5953aaa95c
juan060599 <juansilva@orientefarma.com.br>
2023-02-06 12:26:23 -0300

    Iniciando o Projeto
    Iniciando o Projeto

---
+++ Farmacia.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Farmacia.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ Farmacia.Repositorio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Farmacia.Repositorio
+{
+    public class Class1
+    {
+    }
+}
